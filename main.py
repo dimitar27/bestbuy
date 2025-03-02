819793e5487f84479abec26b59a0c017e47370ed
Dimitar <dimi.dimitrow27@gmail.com>
2025-03-02 21:42:16 +0100

    Implement store menu with order processing

---
+++ main.py
@@ -0,0 +1,88 @@
+from products import Product
+from store import Store
+
+
+def start(store_object):
+    """Handles the store menu and user interactions."""
+    while True:
+        print("\n   Store Menu")
+        print("   ----------")
+        print("1. List all products in store")
+        print("2. Show total amount in store")
+        print("3. Make an order")
+        print("4. Quit")
+
+        try:
+            user_input = int(input("Please choose a number: "))
+            if user_input < 1 or user_input > 4:
+                raise ValueError
+        except ValueError:
+            print("Error with your choice! Try again!")
+            continue
+
+        if user_input == 1:
+            print("------")
+            for index, product in enumerate(store_object.get_all_products(), start=1):
+                print(f"{index}. {product.show()}")
+            print("------")
+
+        elif user_input == 2:
+            print(f"Total of {store_object.get_total_quantity()} in store")
+
+        elif user_input == 3:
+            shopping_list = []
+            product_mapping = {}
+
+            print("\nAvailable Products:")
+            for index, product in enumerate(store_object.get_all_products(), start=1):
+                print(f"{index}. {product.show()}")
+                product_mapping[index] = product
+
+            print("\nWhen you want to finish order, enter empty text.")
+
+            while True:
+
+                user_product_choice = input("Which product # do you want? ")
+                if user_product_choice == "":
+                    break
+
+                try:
+                    user_product_choice = int(user_product_choice)
+                    product_amount = int(input("What amount do you want? "))
+
+                    selected_product = product_mapping.get(user_product_choice)
+
+                    if selected_product is None:
+                        print("Error adding product!")
+                        continue
+
+                    shopping_list.append((selected_product, product_amount))
+                    print("Product added to list!\n")
+
+                except ValueError:
+                    print("Error adding product!")
+            try:
+                total_payment = store_object.order(shopping_list)
+                print(f"********")
+                print(f"Order made! Total payment: ${total_payment:.2f}")
+            except ValueError:
+                print("Error while making order! Quantity larger than what exists")
+
+
+        elif user_input == 4:
+            break
+
+
+def main():
+    product_list = [
+        Product("MacBook Air M2", price=1450, quantity=100),
+        Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+        Product("Google Pixel 7", price=500, quantity=250),
+    ]
+
+    best_buy = Store(product_list)
+
+    start(best_buy)
+
+if __name__ == "__main__":
+    main()
